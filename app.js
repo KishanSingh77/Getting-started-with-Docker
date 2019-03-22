da6d23c59125c3052333e0a109a4e766586dc297
bigLifeGit <vivekthejoker777@gmail.com>
2019-03-22 08:26:54 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+// Constants
+const PORT = 3060;
+const HOST = "0.0.0.0";
+
+// App
+const app = express();
+function mymesage() {
+  return "Hi Everyone.This is Anvesh Mekala and I am building a basic nodeapp using docker";
+}
+app.get("/", (req, res) => {
+  res.send(mymesage());
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
---
+++ dockerfile
@@ -0,0 +1,9 @@
+FROM node:8
+WORKDIR /app
+COPY package*.json /app/
+RUN npm install
+COPY . /app/
+EXPOSE 3060
+CMD ["node","app.js"]
+
+
